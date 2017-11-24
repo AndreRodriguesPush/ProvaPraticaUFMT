718fec06de23af859529696e415d2645cd9f633a
AndreThiagoPR <andrethyagopr@gmail.com>
2017-11-24 19:29:24 -0200

    Fazendo Classe Conexao PDO

--- item5/conexaopdo.php
+++ item5/conexaopdo.php
@@ -1,5 +1,21 @@
 <?php
-$pathDB = "dblib:host=127.0.0.1;dbname=item5ufmt;charset=UTF-8";
-$user = "root";
-$pwd = " ";
-$db = new PDO($pathDB,$user,$pwd);
+
+
+
+try {
+    
+    //$pathDB = "dblib:host=127.0.0.1;dbname=item5ufmt;charset=UTF-8";
+    $pathDB = 'dblib:host=localhost;dbname=db_provaufmt;charset=UTF-8';
+    $userName = 'root';
+    $pwd = " ";
+    //$conn = new PDO($pathDB,$user,$pwd);
+    $conn = new PDO('dblib:host=127.0.0.1:8080;dbname=db_provaufmt;charset=UTF-8',$userName,$pwd);
+    
+    echo "Conectado !!";
+    
+    
+} catch (Exception $e) {
+    
+    echo "Não Conseguimos conectar ao DB - erro:".$e->getMessage();
+    
+}
